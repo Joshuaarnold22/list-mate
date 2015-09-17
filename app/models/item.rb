a656d1a737dacc55f59f9f5eef0f62def1cc6114
Josh Arnold <joshuaarnold22@gmail.com>
2015-09-17 11:45:47 -0400

    Testing models in the controler

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  list_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Item < ActiveRecord::Base
+
+  belongs_to :list
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessor :name, :email
+
+  has_many :items
+end

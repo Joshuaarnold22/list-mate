958bd9075db7d8eba9e07c7af0b66dd625d00944
Josh Arnold <joshuaarnold22@gmail.com>
2015-09-19 21:26:02 -0400

    Create from the command line complete

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,24 @@
+class Api::ListsController < ApiController
+  before_action :authenticated?
+
+  def index
+    lists = List.all
+    render json: lists, each_serializer: ListSerializer
+  end
+
+  def create
+    list = List.new(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entry
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
+
+end
